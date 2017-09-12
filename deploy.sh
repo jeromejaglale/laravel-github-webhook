3d43eb543de33f44e7c2fb83c084e29b094dd631
Jerome Jaglale <jerome_jaglale@sfu.ca>
2017-09-12 16:09:45 -0700

    minor

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 # activate maintenance mode
-############################################
 php artisan down
 
 # update source code
-############################################
 git pull
 
 # update PHP dependencies
-############################################
 export COMPOSER_HOME='/tmp/composer'
 composer install --no-interaction --no-dev --prefer-dist
 	# --no-interaction	Do not ask any interactive question
@@ -17,10 +14,8 @@ composer install --no-interaction --no-dev --prefer-dist
 	# --prefer-dist		Forces installation from package dist even for dev versions.
 
 # update database
-############################################
 php artisan migrate --force
-	# --force			Force the operation to run when in production.
+	# --force			Required to run when in production.
 
 # stop maintenance mode
-############################################
 php artisan up
